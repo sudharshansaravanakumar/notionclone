fad1845b5a4943fe8e61f41a0fe41921d8241fa6
Sudharshan <sudharshan.rangas@gmail.com>
2024-10-27 22:31:46 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { NotionCloneComponent } from "@/components/notion-clone";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <NotionCloneComponent />  
+    </>   
+  );
+}
